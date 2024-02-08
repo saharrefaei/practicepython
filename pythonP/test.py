cd0dba64b3c2bf4a5cabf754905b0142e9bb63b4
Your Correct Username <saharrefaei1999@gmail.com>
2024-02-08 13:25:30 +0100

    password generator

--- pythonP/test.py
+++ pythonP/test.py
@@ -1,35 +1,13 @@
-# Write a Python script that extracts the MAC address (b4:6d:83:77:85:f3) from the string.
-# `my_str = 'wlo1 Link encap:Ethernet HWaddr b4:6d:83:77:85:f3'
-# print(my_str[32::])`
-# ___________________________________________________
+import random
+import string
 
-#Display the following strings literally:
-# It displayed: "You've got an error!"
-# my_str2="\"You've got an error!\""
+char = (string.ascii_letters+string.ascii_lowercase+string.ascii_uppercase+string.digits)
 
-# print(my_str2)
-# ___________________________________________________
-    # Write a Python script to get a string made of the first and the last 2 chars from a given string entered by the user.
-# my_str3=input("enter character 1")
-# my_str4=input("enter character 2")
+password = ''
+length = int(input ( 'enter the length of your password : '))
 
-# print(my_str3 + my_str4)
-# ___________________________________________________
-
-# my_str4=input("enter string")
-# char = my_str4[0]
-# replace = my_str4.replace(char , '%')
-# print(replace)
-
-# ___________________________________________________
-
-# n = int(input('Enter the nth index char to remove:'))
-# my_str = input('Enter the string to change:')
-
-# change = my_str.replace(my_str[n] , 'X')
-# print(change)
-
-n = 12384756982
-n_comma = f'{n:,}'
-
-print(n_comma)
+for _ in range(length):
+    characters = random.choice(char)
+    password+=characters
+    
+print(f"your password is {password}")
