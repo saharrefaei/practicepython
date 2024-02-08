3d6cd30ef5e9311089a77da37a7984da2b34a85b
Your Correct Username <saharrefaei1999@gmail.com>
2024-02-08 16:20:27 +0100

    mutual name

--- pythonP/test.py
+++ pythonP/test.py
@@ -1,6 +1,8 @@
-lis1=['sahar','aref','samaneh','ali']
+lis1=['sahar','aref','sAmaneh','ali']
 lis2=['maman','baba','samaneh','sahar']
-
-mutualName=[name for name in lis1 if name in lis2]
+list1=[i.lower() for i in lis1]
+list2=[i.lower() for i in lis2]
+print(list1,list2)
+mutualName=[name for name in list1 if name in list2]
 
 print(mutualName)
