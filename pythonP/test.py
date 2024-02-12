4184c4d1db9b4f8425f6d33b2e3ec2538fdb3985
Your Correct Username <saharrefaei1999@gmail.com>
2024-02-12 21:07:03 +0100

    exc

--- pythonP/test.py
+++ pythonP/test.py
@@ -1,312 +1,116 @@
-# years = [2015, 2016, 2017, 2018, 2019, 2020]
+#     Consider a list of words(strings). Write a Python script that generates a list of tuples where the first element of the tuple is the word in the list and the second element is its length.
 
-# sales = [350000, 400000, 410000, 439000, 500000, 290000]
+#     Use list comprehension if possible.
+
+# Sample List: words = ['Python', 'Java', 'C++', 'Golang', 'Solidity', 'Bash']
+
+# Expected Result: [('Python', 6), ('Java', 4), ('C++', 3), ('Golang', 6), ('Solidity', 8), ('Bash', 4)]
+
+# words = ['Python', 'Java', 'C++', 'Golang', 'Solidity', 'Bash']
+# lisch =[(i , len(i)) for i in words]
+# print(lisch)
+
+# ______________________________________________
+
+# Write a Python program that accepts as input a sequence of words separated by one or more whitespaces and prints out the same words with the letters in reversed order. Do not use list comprehension.
+# string = "I love cat and dogs"
+
+# reversedString = list() 
+# reversedString = string.split(" ")
+# rev = list()
+# for items in reversedString :
+#     rev =' '.join(items[::-1])
+#     print(rev)
 
 
-# Dictionary=dict(zip(years,sales))
+# ______________________________________________
 
-# print(Dictionary)
+# Write a Python program that accepts a hyphen-separated sequence of words as input and prints the words in a hyphen-separated sequence after sorting them alphabetically.
 
-#-------------
+# words = list()
+# words = input("enter the color")
+# wordsList = list()
+# wordsList = words.split("-")
+# print(sorted(wordsList))
 
-#     words = ['Python', 'Java', 'C++', 'Golang', 'Solidity', 'Bash']
-# dictionary ={}
+# ______________________________________________
 
-# for i in words :
-#     length = len(i)
-#     dictionary[i] = length
-    
-#     print(dictionary)
+# Write a program that prompts the user for a long string containing multiple words separated by whitespaces and prints back the same string with the words in backward order.
 
-#-------------
+# str = "my name is laxmi"
+# revresestr=str.split(" ")
+# print(revresestr[::-1])
 
-# d1 = {'x': 5, 'a': 3, 'c': 2, 'b': 0}
-# sortedKey = sorted(d1.items())
-# print(sortedKey)
+# ______________________________________________
 
-# sortedlist = {}
-# for key, value in sortedKey:
-#     sortedlist[key]=value
+# Write a Python script that finds all numbers that are divisible by 7 but are not a multiple of 5, between 1500 and 3200 (both included).
+# numbers = input("enter").split("-")
+# num=list()
+# for i in numbers :
+#     intnum = int(i)
+#     if intnum % 7 == 0 and intnum % 5 != 0 :
+#        num.append(intnum)
 
-#-------------
+# print(num)
 
-# d1 = {'x': 5, 'a': 3, 'c': 2, 'b': 0}
+# ______________________________________________
 
-# for k in sorted(d1.keys()):
-#     print(f'{k} -> {d1[k]}')
+    # Write a Python script that validates an email address by writing "Valid email!" or "Invalid email!".
 
-#-------------
-# employees = {
-#     'John': ('London', 4000, 28), 
-#     'Maria':('Zagreb', 3800, 40), 
-#     'Diana': ('NYC', 3500, 31)
-#              }
+    # If the email is not valid the script should display why it's not valid.
 
-# sortiation = sorted(employees.items() , key=lambda item:item [0] , reverse=True)
-# print(sortiation)
+    # We consider a valid email address if:
 
-#-------------
-# COUNTRY = {
-# "MX":"MEXICO",
-# "RW":"RWANDA",
-# "BI":"BURUNDI",
-# "PR":"PUERTO RICO",
-# "GM":"GAMBIA",
-# "SR":"SURINAME",
-# "KG":"KYRGYZSTAN",
-# "GA":"GABON",
-# "CK":"COOK ISLANDS",
-# "VN":"VIET NAM",
-# "UZ":"UZBEKISTAN",
-# "BG":"BULGARIA",
-# "JP":"JAPAN",
-# "KP":"KOREA, DEMOCRATIC PEOPLE'S REPUBLIC OF",
-# "PL":"POLAND",
-# "BB":"BARBADOS",
-# "CF":"CENTRAL AFRICAN REPUBLIC",
-# "GI":"GIBRALTAR",
-# "AE":"UNITED ARAB EMIRATES",
-# "MY":"MALAYSIA",
-# "KI":"KIRIBATI",
-# "RU":"RUSSIAN FEDERATION",
-# "MQ":"MARTINIQUE",
-# "SB":"SOLOMON ISLANDS",
-# "TL":"TIMOR-LESTE",
-# "VI":"VIRGIN ISLANDS, U.S.",
-# "FR":"FRANCE",
-# "ZA":"SOUTH AFRICA",
-# "NE":"NIGER",
-# "IE":"IRELAND",
-# "MG":"MADAGASCAR",
-# "TC":"TURKS AND CAICOS ISLANDS",
-# "BW":"BOTSWANA",
-# "KY":"CAYMAN ISLANDS",
-# "JO":"JORDAN",
-# "MR":"MAURITANIA",
-# "NI":"NICARAGUA",
-# "KM":"COMOROS",
-# "FK":"FALKLAND ISLANDS (MALVINAS)",
-# "SH":"SAINT HELENA, ASCENSION AND TRISTAN DA CUNHA",
-# "PF":"FRENCH POLYNESIA",
-# "DK":"DENMARK",
-# "PT":"PORTUGAL",
-# "NR":"NAURU",
-# "DM":"DOMINICA",
-# "GH":"GHANA",
-# "GP":"GUADELOUPE",
-# "CU":"CUBA",
-# "GR":"GREECE",
-# "AS":"AMERICAN SAMOA",
-# "AI":"ANGUILLA",
-# "LS":"LESOTHO",
-# "TJ":"TAJIKISTAN",
-# "AW":"ARUBA",
-# "GY":"GUYANA",
-# "LC":"SAINT LUCIA",
-# "GG":"GUERNSEY",
-# "TG":"TOGO",
-# "OM":"OMAN",
-# "NL":"NETHERLANDS",
-# "MT":"MALTA",
-# "MD":"MOLDOVA, REPUBLIC OF",
-# "EC":"ECUADOR",
-# "FM":"MICRONESIA, FEDERATED STATES OF",
-# "CA":"CANADA",
-# "TT":"TRINIDAD AND TOBAGO",
-# "CN":"CHINA",
-# "CM":"CAMEROON",
-# "BD":"BANGLADESH",
-# "LK":"SRI LANKA",
-# "CO":"COLOMBIA",
-# "LT":"LITHUANIA",
-# "DJ":"DJIBOUTI",
-# "CX":"CHRISTMAS ISLAND",
-# "TF":"FRENCH SOUTHERN TERRITORIES",
-# "TM":"TURKMENISTAN",
-# "SC":"SEYCHELLES",
-# "MK":"MACEDONIA, THE FORMER YUGOSLAV REPUBLIC OF",
-# "TH":"THAILAND",
-# "LA":"LAO PEOPLE'S DEMOCRATIC REPUBLIC",
-# "BE":"BELGIUM",
-# "UY":"URUGUAY",
-# "MV":"MALDIVES",
-# "SZ":"SWAZILAND",
-# "MN":"MONGOLIA",
-# "PK":"PAKISTAN",
-# "BZ":"BELIZE",
-# "AT":"AUSTRIA",
-# "WF":"WALLIS AND FUTUNA",
-# "GD":"GRENADA",
-# "RO":"ROMANIA",
-# "SJ":"SVALBARD AND JAN MAYEN",
-# "CV":"CAPE VERDE",
-# "UG":"UGANDA",
-# "CR":"COSTA RICA",
-# "HM":"HEARD ISLAND AND MCDONALD ISLANDS",
-# "TN":"TUNISIA",
-# "MC":"MONACO",
-# "MP":"NORTHERN MARIANA ISLANDS",
-# "SN":"SENEGAL",
-# "PW":"PALAU",
-# "VC":"SAINT VINCENT AND THE GRENADINES",
-# "ST":"SAO TOME AND PRINCIPE",
-# "DO":"DOMINICAN REPUBLIC",
-# "EG":"EGYPT",
-# "TZ":"TANZANIA, UNITED REPUBLIC OF",
-# "SV":"EL SALVADOR",
-# "TR":"TURKEY",
-# "SG":"SINGAPORE",
-# "UM":"UNITED STATES MINOR OUTLYING ISLANDS",
-# "KR":"KOREA, REPUBLIC OF",
-# "PG":"PAPUA NEW GUINEA",
-# "GQ":"EQUATORIAL GUINEA",
-# "US":"UNITED STATES",
-# "BF":"BURKINA FASO",
-# "AM":"ARMENIA",
-# "TD":"CHAD",
-# "NP":"NEPAL",
-# "IT":"ITALY",
-# "IO":"BRITISH INDIAN OCEAN TERRITORY",
-# "ZW ":"ZIMBABWE",
-# "HU":"HUNGARY",
-# "BR":"BRAZIL",
-# "IN":"INDIA",
-# "PH":"PHILIPPINES",
-# "TW":"TAIWAN, PROVINCE OF CHINA",
-# "AO":"ANGOLA",
-# "MH":"MARSHALL ISLANDS",
-# "NO":"NORWAY",
-# "JE":"JERSEY",
-# "VU":"VANUATU",
-# "EE":"ESTONIA",
-# "AF":"AFGHANISTAN",
-# "AX":"ALAND ISLANDS",
-# "GN":"GUINEA",
-# "FO":"FAROE ISLANDS",
-# "SE":"SWEDEN",
-# "SL":"SIERRA LEONE",
-# "LB":"LEBANON",
-# "MO":"MACAO",
-# "IR":"IRAN, ISLAMIC REPUBLIC OF",
-# "CG":"CONGO",
-# "SM":"SAN MARINO",
-# "NA":"NAMIBIA",
-# "CI":"COTE D'IVOIRE",
-# "GL":"GREENLAND",
-# "VE":"VENEZUELA, BOLIVARIAN REPUBLIC OF",
-# "VG":"VIRGIN ISLANDS, BRITISH",
-# "BH":"BAHRAIN",
-# "ZM":"ZAMBIA",
-# "HR":"CROATIA",
-# "MZ":"MOZAMBIQUE",
-# "KW":"KUWAIT",
-# "MA":"MOROCCO",
-# "DZ":"ALGERIA",
-# "AQ":"ANTARCTICA",
-# "AU":"AUSTRALIA",
-# "PN":"PITCAIRN",
-# "QA":"QATAR",
-# "AL":"ALBANIA",
-# "BN":"BRUNEI DARUSSALAM",
-# "NZ":"NEW ZEALAND",
-# "SA":"SAUDI ARABIA",
-# "RE":"REUNION",
-# "HK":"HONG KONG",
-# "CD":"CONGO, THE DEMOCRATIC REPUBLIC OF THE",
-# "MW":"MALAWI",
-# "CZ":"CZECH REPUBLIC",
-# "DE":"GERMANY",
-# "AD":"ANDORRA",
-# "LU":"LUXEMBOURG",
-# "CY":"CYPRUS",
-# "TO":"TONGA",
-# "FJ":"FIJI",
-# "GT":"GUATEMALA",
-# "LV":"LATVIA",
-# "ES":"SPAIN",
-# "SI":"SLOVENIA",
-# "IM":"ISLE OF MAN",
-# "BS":"BAHAMAS",
-# "RS":"SERBIA",
-# "WS":"SAMOA",
-# "NC":"NEW CALEDONIA",
-# "SY":"SYRIAN ARAB REPUBLIC",
-# "MS":"MONTSERRAT",
-# "GB":"UNITED KINGDOM",
-# "PM":"SAINT PIERRE AND MIQUELON",
-# "CL":"CHILE",
-# "MF":"SAINT MARTIN",
-# "SO":"SOMALIA",
-# "BJ":"BENIN",
-# "YE":"YEMEN",
-# "TV":"TUVALU",
-# "GE":"GEORGIA",
-# "BM":"BERMUDA",
-# "GS":"SOUTH GEORGIA AND THE SOUTH SANDWICH ISLANDS",
-# "AN":"NETHERLANDS ANTILLES",
-# "BY":"BELARUS",
-# "FI":"FINLAND",
-# "BV":"BOUVET ISLAND",
-# "LR":"LIBERIA",
-# "KH":"CAMBODIA",
-# "LY":"LIBYAN ARAB JAMAHIRIYA",
-# "MU":"MAURITIUS",
-# "GU":"GUAM",
-# "ER":"ERITREA",
-# "LI":"LIECHTENSTEIN",
-# "SD":"SUDAN",
-# "PA":"PANAMA",
-# "IL":"ISRAEL",
-# "EH":"WESTERN SAHARA",
-# "KE":"KENYA",
-# "CC":"COCOS (KEELING) ISLANDS",
-# "IS":"ICELAND",
-# "GF":"FRENCH GUIANA",
-# "MM":"MYANMAR",
-# "HT":"HAITI",
-# "NF":"NORFOLK ISLAND",
-# "ML":"MALI",
-# "PY":"PARAGUAY",
-# "KZ":"KAZAKHSTAN",
-# "CH":"SWITZERLAND",
-# "BA":"BOSNIA AND HERZEGOVINA",
-# "BO":"BOLIVIA, PLURINATIONAL STATE OF",
-# "UA":"UKRAINE",
-# "BL":"SAINT BARTHELEMY",
-# "AZ":"AZERBAIJAN",
-# "BT":"BHUTAN",
-# "VA":"HOLY SEE (VATICAN CITY STATE)",
-# "PE":"PERU",
-# "AR":"ARGENTINA",
-# "TK":"TOKELAU",
-# "AG":"ANTIGUA AND BARBUDA",
-# "KN":"SAINT KITTS AND NEVIS",
-# "PS":"PALESTINIAN TERRITORY, OCCUPIED",
-# "ID":"INDONESIA",
-# "GW":"GUINEA-BISSAU",
-# "HN":"HONDURAS",
-# "NG":"NIGERIA",
-# "IQ":"IRAQ",
-# "JM":"JAMAICA",
-# "NU":"NIUE",
-# "ET":"ETHIOPIA",
-# "ME":"MONTENEGRO",
-# "SK":"SLOVAKIA",
-# "YT":"MAYOTTE"
-# }
-# lengths =[len(i) for i in COUNTRY.values()]
+    #     it has at least 6 characters but no more than 16.
 
-# max= [i for i in COUNTRY.values() if len(i) ==lengths ]
-# print(print(max))
+    #     it contains both . and @
 
-#---------------------
+    #     it does not contain any of the following characters:'[]{}()$*'
+# email = list()
+# email = input("enter email addres : ")
+# invalidChar = ["[" , ']' , "*"]
+# emailLength = len(email)
 
-# years = [2015, 2016, 2017, 2018, 2019, 2020]
+# for char1 in email:
+#     for invalid1 in invalidChar:
+#         if char1 == invalid1 :
+#             print("Invalid character found:", char1)
+#             break
+                 
+#         if emailLength < 6  or emailLength > 16 :
+#             print(" the length of email is incorrect")
+#             break
+        
+#         else : 
+#             print("valid email")
+#             break
+     
+            
+     # ______________________________________________
+# Consider the following 2 Lists: names = ["Dan", "John", "Diana"] and phones = [11111, 2222, 3333].
 
-# sales = [350000, 400000, 410000, 439000, 500000, 290000]
+# Create a set that contains the elements of the 2 lists in pairs.
+   
+# names = ["Dan", "John", "Diana"] 
+# phones = [11111, 2222, 3333]
 
-# mix = dict(zip(years,sales))
+# newchar =dict(zip(names,phones))
+# print(newchar)
 
-# print(mix)
+     # ______________________________________________
+# Consider this dictionary. Print a sorted view of the dictionary by the third field of its values, in reverse order.
 
-#---------------------
+# employees = {'John': ('London', 4000, 28), 'Maria': ('Zagreb', 3800, 40), 'Diana': ('NYC', 3500, 31)}
+# view = sorted(employees.items() , key=lambda x:x[1] , reverse=True)
+# print(view)
+
+     # ______________________________________________
+
+
+# def tree(height):
+#     firstline = height * 2 - 1
+#     while firstline > 0:
+#         print("*" * firstline)
+#         firstline -= 2
+
+# tree(5)
